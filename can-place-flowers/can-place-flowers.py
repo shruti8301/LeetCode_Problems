34855776a5a90823c78a3f7835a8d624fd7ab248
Shruti Gupta <shruti8301@gmail.com>
2021-10-21 23:37:35 +0530

    Time: 240 ms (23.88%), Space: 14.7 MB (8.49%) - LeetHub

---
+++ can-place-flowers/can-place-flowers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        bed=[0]+flowerbed+[0]
+        for i in range(1,len(flowerbed)+1):
+            if bed[i]==bed[i-1]==bed[i+1]:
+                bed[i]=1
+                n-=1
+            if n<=0:
+                return True
+        return False
