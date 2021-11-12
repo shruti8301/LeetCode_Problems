a50b0d88a1e4a7e4acf7f4d6edb7d17992893d4b
Shruti Gupta <shruti8301@gmail.com>
2021-11-12 22:50:13 +0530

    Time: 40 ms (73.71%), Space: 14.2 MB (90.87%) - LeetHub

---
+++ baseball-game/baseball-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def calPoints(self, ops: List[str]) -> int:
+        arr=[]
+        for i in ops:
+            if i=="+":
+                arr.append(int(arr[-1])+int(arr[-2]))
+            elif i=="D":
+                arr.append(2*arr[-1])
+            elif i=="C":
+                arr.pop()
+            else:
+                arr.append(int(i))
+        return sum(arr)
+                
+        
