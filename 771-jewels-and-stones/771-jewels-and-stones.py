9a53c7362d8c954e6d571de9325b6eae44844262
Shruti Gupta <shruti8301@gmail.com>
2022-01-15 20:24:23 +0530

    Time: 39 ms (30.53%), Space: 14.2 MB (73.71%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,7 +1,5 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        if not jewels or not stones:
-            return 0
         a=0
         for i in jewels:
             if i in stones:
