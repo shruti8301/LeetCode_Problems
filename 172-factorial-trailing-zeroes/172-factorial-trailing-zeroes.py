861df12fc5ccec3f8c51f6533a3b07693eb52a96
Shruti Gupta <shruti8301@gmail.com>
2022-02-23 22:08:01 +0530

    Time: 47 ms (59.22%), Space: 13.8 MB (98.86%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
@@ -1,13 +1,5 @@
 class Solution:
-    def factorial(self,n):
-        if n == 0 or n == 1:
-            return 1
-        else:
-            fact = 1
-            while(n > 1):
-                fact *= n
-                n -= 1
-            return fact
+    
     def trailingZeroes(self, n: int) -> int:
         if n<5:
             return 0
