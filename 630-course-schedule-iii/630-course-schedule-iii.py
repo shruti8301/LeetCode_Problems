9b9abc5723347f95193349202835e00d924d61dc
Shruti Gupta <shruti8301@gmail.com>
2022-06-23 21:24:32 +0530

    Time: 1276 ms (22.66%), Space: 19.3 MB (97.25%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.py
+++ 630-course-schedule-iii/630-course-schedule-iii.py
@@ -2,13 +2,13 @@ class Solution:
 	def scheduleCourse(self, courses: List[List[int]]) -> int:
 		# Time O(nlogn), Space(O(n))
 		courses.sort(key=lambda x:x[1])
-		pq = []
+		arr = []
 		time = 0
 
-		for start,end in courses:
-			heappush(pq, -start)
-			time += start
-			if time > end:
-				time += heappop(pq)
+		for i,j in courses:
+			heappush(arr, -i)
+			time += i
+			if time > j:
+				time += heappop(arr)
 
-		return len(pq)
+		return len(arr)
