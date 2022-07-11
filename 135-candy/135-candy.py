c43bf7a09880ea6720936537471a57d29bdff792
Shruti Gupta <shruti8301@gmail.com>
2022-07-11 22:21:25 +0530

    Time: 259 ms (58.70%), Space: 16.8 MB (74.20%) - LeetHub

--- 135-candy/135-candy.py
+++ 135-candy/135-candy.py
@@ -1,20 +1,11 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        z = sorted([(i, r) for i, r in enumerate(ratings)], key=lambda x: (x[1], x[0]))
-        d = [-1 for _ in range(len(ratings))]
-
-        for i, v in z:
-            c = 0
-            # check left
-            if i > 0 and d[i-1] != -1 and ratings[i-1] < v:
-                c = d[i-1] + 1
-            # check right
-            if i < len(ratings) -1 and d[i+1] != -1 and ratings[i+1] < v:
-                c = max(c, d[i+1]+1)
-
-            if c == 0:
-                c = 1
-
-            d[i] = c
-
-        return sum(d)
+        n=len(ratings)
+        candy=[1]*n
+        for i in range(1,n):
+            if ratings[i]>ratings[i-1]:
+                candy[i]=candy[i-1]+1
+        for i in range(n-2,-1,-1):
+            if ratings[i]>ratings[i+1]:
+                candy[i]=max(candy[i],candy[i+1]+1)
+        return sum(candy)
